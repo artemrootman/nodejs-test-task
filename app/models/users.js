afbec89c4ced8bcabee403ee8dce41b7272cb626
Artyom Tyuev <drojfs@gmail.com>
2022-06-09 23:53:05 +0300

    add email validation

--- app/models/users.js
+++ app/models/users.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose')
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const usersSchema = new mongoose.Schema(
   {
     email: {
       type: String,
+      trim: true,
+      lowercase: true,
       required: true,
       unique: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
       max: [50, 'Max Length is 50 characters'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     ua: {
       type: String,
